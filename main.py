4aabf7c127f9f7ab24f02893d75bff57cea60ea4
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-26 18:38:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+number = 1
+
+while number < 100:
+    print(number)
+    number = number * 2 # pro zopakování: hvězdička slouží k násobení
+
+# Kolik iterací proběhne ve while cyklu?
+  # 
+
+# Co by se stalo, pokud bych zapomněla zapsat příkaz na řádku 5 (tedy změnit hodnotu proměnné)?
+  #
+
+# Co by se stalo, pokud bych zapomněla na dvojtečku na řádku 3?
+  #
+
+# Co by se stalo, pokud bych zapomněla odsadit příkaz za dvojtečkou (tedy např. řádek 4)?
+  #
